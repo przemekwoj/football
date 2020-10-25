878708e3e50506259ac43a606a2e26b006059b10
Przemek <przemoemo@poczta.onet.pl>
2020-10-25 13:51:07 +0100

    test commit

---
+++ src/test/java/com/pw/football/FootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
