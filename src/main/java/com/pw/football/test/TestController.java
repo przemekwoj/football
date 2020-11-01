11067e7481509c851ac1d49b291aae3990d97945
przemekwoj <przemoemo@poczta.onet.pl>
2020-11-01 11:51:58 +0100

    test4

--- src/main/java/com/pw/football/test/TestController.java
+++ src/main/java/com/pw/football/test/TestController.java
@@ -3,12 +3,14 @@ package com.pw.football.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class TestController {
 
     @GetMapping("test")
     public String test(){
-        System.out.println("test2");
         return "test";
     }
 }
