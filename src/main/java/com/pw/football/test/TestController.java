5410015af4c33302e8e241a84a544ecf95c3907f
przemekwoj <przemoemo@poczta.onet.pl>
2020-11-01 15:21:36 +0100

    create application skeleton

---
+++ src/main/java/com/pw/football/test/TestController.java
@@ -0,0 +1,16 @@
+package com.pw.football.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test(){
+        return "test";
+    }
+}
---
+++ src/test/java/com/pw/football/FootballApplicationTests.java
@@ -0,0 +1,22 @@
+package com.pw.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+class FootballApplicationTests {
+
+	@Test
+	void shouldFail() {
+		assertTrue(true == false);
+	}
+
+	@Test
+	void shouldPass(){
+		assertTrue(true == true);
+	}
+
+}
